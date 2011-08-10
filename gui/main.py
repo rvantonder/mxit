faeb0f649677a83548199a88e5bd24eb23854b4c
Rai <rvantonder@gmail.com>
2011-08-10 17:12:01 +0200

    trying to get other logins to work

--- gui/main.py
+++ gui/main.py
@@ -5,7 +5,8 @@ import sys
 if __name__ == '__main__':
     try:
       app = QtGui.QApplication(sys.argv)
-      gui = ClientForm('41.191.124.10', 9119, '0716223917', '3NZjbv5S02qK0YkoOjiQrw==')
+      #gui = ClientForm('41.191.124.10', 9119, '0716223917', '3NZjbv5S02qK0YkoOjiQrw==')
+      gui = ClientForm('41.191.124.10', 9119, 'flat.eric', r'G/gXAtYw8VdCZNOa0AAsKA==') 
       gui.show()
       sys.exit(app.exec_())
     except IndexError:
--- gui/messages.py
+++ gui/messages.py
@@ -1,8 +1,11 @@
+def messageToHex(msg):
+  print ' '.join(map(lambda x:hex(ord(x)), msg))
+
 def construct_msg(data):
+  print 'DATA'
+  print data
   msg = '\1'.join(map(lambda x: str(x) if isinstance(x,int) else x, data))
   return msg
-
-  
 
 class GenericMessage:
   def __init__(self, conn, cmd, data):
@@ -14,14 +17,16 @@ class GenericMessage:
   def send(self):
     rawstring = construct_msg(self.data)
    
-    messageToSend = "id=" + self.conn.id + "\0cm=" + str(self.cmd) + "\0ms=" + rawstring
+    messageToSend = "id=" + self.conn.id + "\0cm=" + str(self.cmd) + "\0ms=" + rawstring + '\1' + chr(0x30) 
     messageToSend = "ln=" + str(len(messageToSend)) + "\0" + messageToSend
 
-    print 'sending ',messageToSend
+    print 'WRONG'
+    print messageToSend
+    messageToHex(messageToSend)
 
     self.conn.socket.sendall(messageToSend)
     
 
-class LoginMessage:
+class LoginMessage(GenericMessage):
   def __init__(self, conn):
     GenericMessage.__init__(self, conn, 1, conn.properties.values())  #enum with Mxit.LOGIN = 1 
